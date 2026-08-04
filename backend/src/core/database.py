49c062dc92b20342c175950f4fdc16af6790ca09
Wiltort <wiltort21@gmail.com>
2026-08-04 09:03:15 +0500

    Refactor imports in database.py to follow PEP 8 style guidelines

--- backend/src/core/database.py
+++ backend/src/core/database.py
@@ -1,5 +1,6 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
+
 from src.core.config import settings
 
 async_engine = create_async_engine(settings.database_url)

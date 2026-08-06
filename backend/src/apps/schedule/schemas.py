7af4b7adf3a604529036bc2630739221fa78bdbf
Wiltort <wiltort21@gmail.com>
2026-08-06 11:51:57 +0500

    Add schema for creating ScheduleImage

--- backend/src/apps/schedule/schemas.py
+++ backend/src/apps/schedule/schemas.py
@@ -1,6 +1,7 @@
+import uuid
 from datetime import datetime
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, ConfigDict, Field
 from src.enums.schedule import DayOfWeek
 
 
@@ -21,16 +22,27 @@ class ScheduleImageBase(BaseModel):
     day_of_week: DayOfWeek = Field(
         ..., description="День недели", examples=[DayOfWeek.MONDAY]
     )
-    create_at: datetime = Field(
-        ..., description="Дата создания", examples=[datetime.now()]
-    )
-    update_at: datetime = Field(
-        ..., description="Дата обновления", examples=[datetime.now()]
-    )
 
 
-class ScheduleImageCreate(ScheduleImageBase):
-    pass
+class ScheduleImageCreate(BaseModel):
+    name: str | None = Field(
+        default=None,
+        description="Название расписания",
+        examples=["Расписание 1"],
+        max_length=255,
+    )
+    image: str = Field(
+        ...,
+        description="Изображение расписания",
+        examples=["image.png"],
+        max_length=255,
+    )
+    is_active: bool | None = Field(
+        default=None, description="Активное расписание", examples=[True]
+    )
+    day_of_week: DayOfWeek | None = Field(
+        default=None, description="День недели", examples=[DayOfWeek.MONDAY]
+    )
 
 
 class ScheduleImageUpdate(BaseModel):
@@ -48,4 +60,31 @@ class ScheduleImageUpdate(BaseModel):
     )
     is_active: bool | None = Field(
         None, description="Активное расписание", examples=[True]
+    )
+    day_of_week: DayOfWeek | None = Field(
+        None, description="День недели", examples=[DayOfWeek.MONDAY]
+    )
+
+
+class ScheduleImageGet(ScheduleImageBase):
+    id: uuid.UUID = Field(..., description="ID расписания", examples=[uuid.uuid4()])
+    create_at: datetime = Field(
+        ..., description="Дата создания", examples=[datetime.now()]
+    )
+    update_at: datetime = Field(
+        ..., description="Дата обновления", examples=[datetime.now()]
+    )
+
+    model_config = ConfigDict(
+        from_attributes=True,
+        json_schema_extra={
+            "example": {
+                "id": str(uuid.uuid4()),
+                "name": "Расписание 1",
+                "image": "image.png",
+                "is_active": True,
+                "day_of_week": DayOfWeek.MONDAY.value,
+            },
+            "description": "Схема расписания",
+        },
     )

e196c4663437b5cfd7f5e74946a3c5b7809916a1
Wiltort <wiltort21@gmail.com>
2026-08-06 14:58:52 +0500

    add ScheduleImage manager

--- backend/src/apps/schedule/managers.py
+++ backend/src/apps/schedule/managers.py
@@ -1,2 +1,25 @@
+from fastapi import Depends, HTTPException
+from sqlalchemy import insert
+from sqlalchemy.exc import IntegrityError
+from src.core.database import DBDependency
+from src.models import ScheduleImage
+from src.apps.schedule.schemas import ScheduleImageCreate, ScheduleImageGet
+
+
 class ScheduleImageManager:
-    pass
+
+    def __init__(self, db: DBDependency = Depends(DBDependency)) -> None:
+        self.db = db
+        self.model = ScheduleImage
+
+    async def create(self, schedule: ScheduleImageCreate) -> ScheduleImageGet:
+        async with self.db.db_session as session:
+            query = insert(self.model).values(**schedule.model_dump()).returning(self.model)
+
+            try:
+                result = await session.execute(query)
+            except IntegrityError as e:
+                raise HTTPException(status_code=400, detail=str(e))
+            await session.commit()
+            schedule_data = result.scalar_one()
+            return ScheduleImageGet.model_validate(schedule_data)

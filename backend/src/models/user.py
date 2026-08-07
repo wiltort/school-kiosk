f2a2cb9464fdbac6a2e10df70cb11a22d5183f29
Wiltort <wiltort21@gmail.com>
2026-08-07 15:19:17 +0500

    fix: format code

--- backend/src/models/user.py
+++ backend/src/models/user.py
@@ -1,14 +1,13 @@
-from sqlalchemy import Boolean, String, Text  
-from sqlalchemy.orm import Mapped, mapped_column  
+from sqlalchemy import Boolean, String, Text
+from sqlalchemy.orm import Mapped, mapped_column
 
 from src.models.base import Base
 from src.models.mixins import IDMixin, TimestampMixin
 
 
-class User(IDMixin, TimestampMixin, Base):  
+class User(IDMixin, TimestampMixin, Base):
     email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
-    hashed_password: Mapped[str] = mapped_column(Text, unique=False, nullable=False)  
-    is_active: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)  
-    is_superuser: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)  
+    hashed_password: Mapped[str] = mapped_column(Text, unique=False, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    is_superuser: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
     is_verified: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
-    
